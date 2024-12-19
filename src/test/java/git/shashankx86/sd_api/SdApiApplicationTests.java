0d26c5527b1be0db770d7c95d29a32e1f17f5722
shashankx86 <shashankspis20@gmail.com>
2024-12-19 11:04:17 +0530

    init

---
+++ src/test/java/git/shashankx86/sd_api/SdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package git.shashankx86.sd_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
